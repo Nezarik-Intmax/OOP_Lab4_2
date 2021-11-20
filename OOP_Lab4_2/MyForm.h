704b77cb06fb15a794798e14f16a2eac63682acc
Nezarik-Intmax <nezarikintmax@gmail.com>
2021-11-20 23:49:33 +0500

    Test

--- OOP_Lab4_2/MyForm.h
+++ OOP_Lab4_2/MyForm.h
@@ -34,6 +34,19 @@ namespace OOPLab42 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Label^ label1;
+	protected:
+	private: System::Windows::Forms::NumericUpDown^ numericUpDown1;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TrackBar^ trackBar1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::TrackBar^ trackBar2;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::NumericUpDown^ numericUpDown2;
+	private: System::Windows::Forms::TrackBar^ trackBar3;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::NumericUpDown^ numericUpDown3;
+	private: System::Windows::Forms::Label^ label3;
 
 	private:
 		/// <summary>
@@ -48,11 +61,150 @@ namespace OOPLab42 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->numericUpDown1 = (gcnew System::Windows::Forms::NumericUpDown());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->trackBar1 = (gcnew System::Windows::Forms::TrackBar());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->trackBar2 = (gcnew System::Windows::Forms::TrackBar());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->numericUpDown2 = (gcnew System::Windows::Forms::NumericUpDown());
+			this->trackBar3 = (gcnew System::Windows::Forms::TrackBar());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->numericUpDown3 = (gcnew System::Windows::Forms::NumericUpDown());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->trackBar1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->trackBar2))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown2))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->trackBar3))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown3))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 36, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label1->Location = System::Drawing::Point(176, 157);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(56, 55);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"A";
+			// 
+			// numericUpDown1
+			// 
+			this->numericUpDown1->Location = System::Drawing::Point(147, 232);
+			this->numericUpDown1->Name = L"numericUpDown1";
+			this->numericUpDown1->Size = System::Drawing::Size(120, 20);
+			this->numericUpDown1->TabIndex = 1;
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(147, 258);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(120, 20);
+			this->textBox1->TabIndex = 2;
+			// 
+			// trackBar1
+			// 
+			this->trackBar1->Location = System::Drawing::Point(147, 285);
+			this->trackBar1->Name = L"trackBar1";
+			this->trackBar1->Size = System::Drawing::Size(120, 45);
+			this->trackBar1->TabIndex = 3;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 36, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label2->Location = System::Drawing::Point(318, 157);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(56, 55);
+			this->label2->TabIndex = 4;
+			this->label2->Text = L"A";
+			// 
+			// trackBar2
+			// 
+			this->trackBar2->Location = System::Drawing::Point(285, 285);
+			this->trackBar2->Name = L"trackBar2";
+			this->trackBar2->Size = System::Drawing::Size(120, 45);
+			this->trackBar2->TabIndex = 7;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(285, 258);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(120, 20);
+			this->textBox2->TabIndex = 6;
+			// 
+			// numericUpDown2
+			// 
+			this->numericUpDown2->Location = System::Drawing::Point(285, 232);
+			this->numericUpDown2->Name = L"numericUpDown2";
+			this->numericUpDown2->Size = System::Drawing::Size(120, 20);
+			this->numericUpDown2->TabIndex = 5;
+			// 
+			// trackBar3
+			// 
+			this->trackBar3->Location = System::Drawing::Point(424, 285);
+			this->trackBar3->Name = L"trackBar3";
+			this->trackBar3->Size = System::Drawing::Size(120, 45);
+			this->trackBar3->TabIndex = 11;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(424, 258);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(120, 20);
+			this->textBox3->TabIndex = 10;
+			// 
+			// numericUpDown3
+			// 
+			this->numericUpDown3->Location = System::Drawing::Point(424, 232);
+			this->numericUpDown3->Name = L"numericUpDown3";
+			this->numericUpDown3->Size = System::Drawing::Size(120, 20);
+			this->numericUpDown3->TabIndex = 9;
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 36, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label3->Location = System::Drawing::Point(457, 157);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(56, 55);
+			this->label3->TabIndex = 8;
+			this->label3->Text = L"A";
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(687, 483);
+			this->Controls->Add(this->trackBar3);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->numericUpDown3);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->trackBar2);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->numericUpDown2);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->trackBar1);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->numericUpDown1);
+			this->Controls->Add(this->label1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->trackBar1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->trackBar2))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown2))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->trackBar3))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown3))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
 	};
